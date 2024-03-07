809672ad15f77ab627b44f954b861ebf6290f20c
Tr1olll <voicescu.e@yahoo.com>
2024-03-07 14:22:04 +0100

    homepage
    added homepage.html / hompage_style.css / homepage_script.css

---
+++ mBot2_Projekt_v1/src/main/resources/static/homepage_script.js
@@ -0,0 +1,14 @@
+function handleClick(container) {
+    // Code to execute when the container is clicked
+    console.log("Container clicked!");
+}
+
+function handleHover(container) {
+    // Code to execute when the container is hovered
+    container.style.backgroundColor = "lightblue"; // Example: Change background color
+}
+
+function handleLeave(container) {
+    // Code to execute when the mouse leaves the container
+    container.style.backgroundColor = ""; // Reset background color
+}
